198c6b047b7ea2fa77b5fd7e1ac31edf750af2ce
Vatsook1996 <ahmetdianovv@mail.ru>
2023-05-03 17:27:01 +0300

    init commit1

--- src/test/java/com/demoga/tests/TestBase.java
+++ src/test/java/com/demoga/tests/TestBase.java
@@ -10,7 +10,6 @@ public class TestBase {
     static void beforeALL() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
 }
 }
