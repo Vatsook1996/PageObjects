4bbefec3221b53ca78db02154e50707977b6bfbe
Vatsook1996 <ahmetdianovv@mail.ru>
2023-05-25 21:07:10 +0300

    Commit fix2

--- src/test/java/com/demoga/tests/RegistrationWithTestDateTests.java
+++ src/test/java/com/demoga/tests/RegistrationWithTestDateTests.java
@@ -2,58 +2,93 @@ package com.demoga.tests;
 
 import com.demoga.pages.RegistrationPage;
 import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+
 import static com.demoga.tests.TestData.*;
-import static com.demoga.utils.RandomUtils.*;
-public class RegistrationWithTestDateTests extends TestBase{
+import static com.demoga.utils.RandomUtils.getRandomCity;
+import static com.demoga.utils.RandomUtils.getRandomInt;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationWithTestDateTests extends RemoteTestBase {
+
     RegistrationPage registrationPage = new RegistrationPage();
     Faker faker = new Faker();
-    String  firstName = faker.name().firstName(),
+    String firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
             userEmail = faker.internet().emailAddress(),
             userGender = faker.options().option(TestData.gender),
-
             userNumber = 89 + faker.phoneNumber().subscriberNumber(8),
             dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 25)),
             monthOfBirth = faker.options().option(months),
             yearOfBirth = String.valueOf(getRandomInt(1956, 2004)),
             subject = faker.options().option(subjects),
             hobbies = faker.options().option(hobbiess),
-            Picture = faker.options().option(picture),
-                    currentAddress= faker.address().streetAddress(),
+            currentAddress = faker.address().streetAddress(),
             randomState = faker.options().option(states),
             randomCity = getRandomCity(randomState);
 
+    @Tag("remote")
+    @Test
+    @DisplayName("Проверка тестовой формы")
+    void practiceFormTest() {
 
-        @Test
-        void successfulPracticeFormTests() {
-
-            registrationPage.openPage()
-                .removeBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(userGender)
-                .setNumber(userNumber)
-                .setBerthDate(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubjects(subject)
-                .setHobbies(hobbies)
-                    .setPicture(Picture)
-                .setAddress(currentAddress)
-                .setState(randomState)
-                .setCity(randomCity)
-                .clickSubmit();
-
-        registrationPage.verifyResult("Student Name", firstName + " " + lastName)
-                .verifyResult("Student Email", userEmail)
-                .verifyResult("Gender", userGender)
-                .verifyResult("Mobile", userNumber)
-                .verifyResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .verifyResult("Subjects", subject)
-                .verifyResult("Hobbies", hobbies)
-                .verifyResult("Picture", Picture)
-                .verifyResult("Address", currentAddress)
-                .verifyResult("State and City", randomState + " " + randomCity);
- }
+        step("Открываем страницу", () -> {
+            registrationPage.openPage();
+        });
+        step("Закрываем банер и футер", () -> {
+            registrationPage.removeBanners();
+        });
+        step("Вводим имя и фамилию", () -> {
+            registrationPage.setFirstName(firstName);
+            registrationPage.setLastName(lastName);
+        });
+        step("Вводим email", () -> {
+            registrationPage.setUserEmail(userEmail);
+        });
+        step("Выбираем гендер", () -> {
+            registrationPage.setGender(userGender);
+        });
+        step("Вводим номер телефона", () -> {
+            registrationPage.setNumber(userNumber);
+        });
+        step("Вводим дату рождения", () -> {
+            registrationPage.setBerthDate(dayOfBirth, monthOfBirth, yearOfBirth);
+        });
+        step("Выбираем предметы", () -> {
+            registrationPage.setSubjects(subject);
+        });
+        step("Выбираем хобби", () -> {
+            registrationPage.setHobbies(hobbies);
+        });
+        step("Загружаем картинку", () -> {
+            registrationPage.setPicture("P-75.png");
+        });
+        step("Вводим адрес", () -> {
+            registrationPage.setAddress(currentAddress);
+        });
+        step("Выбираем штат", () -> {
+            registrationPage.setState(randomState);
+        });
+        step("Выбираем город", () -> {
+            registrationPage.setCity(randomCity);
+        });
+        step("Кликаем на кнопку Submit", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Проверяем результат", () -> {
+            registrationPage.verifyResult("Student Name", firstName + " " + lastName)
+                    .verifyResult("Student Email", userEmail)
+                    .verifyResult("Gender", userGender)
+                    .verifyResult("Mobile", userNumber)
+                    .verifyResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                    .verifyResult("Subjects", subject)
+                    .verifyResult("Hobbies", hobbies)
+                    .verifyResult("Picture", "P-75.png")
+                    .verifyResult("Address", currentAddress)
+                    .verifyResult("State and City", randomState + " " + randomCity);
+        });
+    }
 }
--- src/test/java/com/demoga/utils/RandomUtils.java
+++ src/test/java/com/demoga/utils/RandomUtils.java
@@ -1,40 +1,40 @@
 package com.demoga.utils;
 
-import static com.demoga.tests.TestData.*;
 
 import com.github.javafaker.Faker;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import static com.demoga.tests.TestData.*;
 
 
-    public class RandomUtils {
-        private static Faker faker = new Faker();
+public class RandomUtils {
+    private static Faker faker = new Faker();
 
-        public static int getRandomInt(int min, int max) {
-            return ThreadLocalRandom.current().nextInt(min, max + 1);
-        }
-
-        public static String getRandomCity(String state) {
-            String city;
-            switch (state) {
-                case "NCR": {
-                    city = faker.options().option(stateNCR);
-                    return city;
-                }
-                case "Uttar Pradesh": {
-                    city = faker.options().option(stateUttarPradesh);
-                    return city;
-                }
-                case "Haryana": {
-                    city = faker.options().option(stateHaryana);
-                    return city;
-                }
-                case "Rajasthan": {
-                    city = faker.options().option(stateRajasthan);
-                    return city;
-                }
-            }
-            return null;
-        }
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
+
+    public static String getRandomCity(String state) {
+        String city;
+        switch (state) {
+            case "NCR": {
+                city = faker.options().option(stateNCR);
+                return city;
+            }
+            case "Uttar Pradesh": {
+                city = faker.options().option(stateUttarPradesh);
+                return city;
+            }
+            case "Haryana": {
+                city = faker.options().option(stateHaryana);
+                return city;
+            }
+            case "Rajasthan": {
+                city = faker.options().option(stateRajasthan);
+                return city;
+            }
+        }
+        return null;
+    }
+}
